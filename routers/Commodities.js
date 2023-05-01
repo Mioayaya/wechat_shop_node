96852c3bb58be9de69a5f79f1909021daebc97ba
Mioayaya <1530020617@qq.com>
2023-05-01 17:18:10 +0800

    多表联查测试

--- routers/Commodities.js
+++ routers/Commodities.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Commodities = require("../models/commodities");
+const Shops = require("../models/shops");
 
 router.get("/home",async (req,res) => {
-  
+
   const commodities = await Commodities.findAll({
     attributes: { exclude: ['id'] }
   });
@@ -24,6 +25,24 @@ router.get("/home",async (req,res) => {
 
   res.send({commoditiesL,commoditiesR})
 
+})
+
+router.get("/test",async (req,res) => {
+  try {
+    // 商店id
+    const { id } = req.query;
+    const shop = await Shops.findOne({
+      where: {shop_id:id}
+    })
+        
+    const commoditityList = await Commodities.findAll({
+      attributes: { exclude: ['id'] },
+      where: {shop_id:shop.shop_id}
+    })
+    res.send({commoditityList})
+  } catch(e) {
+    res.send({err:e})
+  }
 })
 
 module.exports = router;
